010a3c2cb49a6e9686ad62e0b238fc1c1277eb85
James Scalise <jamescscalise@gmail.com>
2019-08-03 19:25:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,11 +9,8 @@ def map(array)
 end
 def reduce(array, starting_point = 0)
   i = 0
-  
   while i<array.length do
     starting_point = yield(starting_point, array[i])
-    
     i+=1
   end
-  
 end
