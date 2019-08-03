173d4947d5ffdbd303ae46997cadfe6c7f776be1
James Scalise <jamescscalise@gmail.com>
2019-08-03 19:36:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,7 +11,7 @@ def reduce(array, starting_point = 0)
   start = array[0] + starting_point
   i = 1
   while i<array.length do
-    starting_point = yield(starting_point, array[i])
+    start = yield(start, array[i])
     i+=1
   end
   return starting_point
